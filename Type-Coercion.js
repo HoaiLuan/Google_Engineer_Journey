cc5164c16473d9371f84cddcf3620967be17b9f1
Tran Hoai Luan <tranhoailuan080623@gmail.com>
2026-05-23 00:11:39 +0700

    feat: complete variables and type coercion practical exercises

---
+++ Type-Coercion.js
@@ -0,0 +1,26 @@
+const result ="Số mục tiêu là:"+9;
+console.log(result); // Output: Số mục tiêu là:9
+
+const result2 ="Kết quả của 10-5 là :" + ("10"-"5");
+console.log(result2); // Output: 5
+
+// đối với trong phép toán true là 1 và false là 0
+const result3 = 10 + true;
+console.log(result3); // Output: 11
+
+const result4 = 10 - false;
+console.log(result4); // Output: 10
+
+// ép kiểu dữ liệu
+const num1 = "5";
+const num2 = "10";
+const sum = Number(num1) + Number(num2);
+console.log(sum); // Output: 15
+
+consologe.log (5=="5");
+
+const myPattern = "Sliding Windows";
+
+if (myPattern) {
+    console.log("Pattern is truthy");
+}
---
+++ Variables.js
@@ -0,0 +1,38 @@
+
+// Biến Lưu trữ dữ liệu kiểu số nguyên
+
+const targetSum = 9;
+
+// Biến lưu trữ dữ liệu kiểu số thực
+let accountBalance = 150.50;
+
+// Biến lưu trữ dữ liệu kiểu chuỗi
+
+const userName = "Trần Hoài Luân";
+
+// Biến lưu trữ dữ liệu kiểu 
+const isLoggedIn = true;
+
+// Biến lưu trữ kiểu dữ liệu kiểu mảng
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+// Biến lưu trữ dữ liệu kiểu đối tượng (object)
+const leedcodePlan = {
+    durationMonths: 12,
+    dailyGoal: 1,
+}
+
+// Biến cấu trúc bảng băm (Map) là một cấu trúc dữ liêu lưu trữ dữ liệu theo cặp key và value, cho phép truy cập nhanh chóng và hiệu quả đến các giá trị dựa trên khóa của chúng.
+const  numToIndexMap = new Map();
+
+// Biến rỗng chưa có giá trị (undefined)
+
+let userAttempts;
+// Biến có giá trị null, biểu thị sự vắng mặt của một giá trị hoặc đối tượng
+
+let CurrentUser = null;
+
+// Biến lưu trữ một hàng xử lý (Function) để giải quyết vấn đề hai số cộng lại bằng một giá trị mục tiêu (Two Sum Problem)
+
+const greetUser = () => "Good morning, " + userName + "!";
